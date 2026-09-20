a9cec032c6d90fe28d541d4a0f4d684c237dcd5e
Vinclwanzl <144253951+Vinclwanzl@users.noreply.github.com>
2026-09-20 18:32:06 +0200

    Submission

---
+++ Aufgabe1/Assignment_A/README.md
@@ -0,0 +1,4 @@
+# Vue 3 + TypeScript + Vite
+
+I used: npm create vite@latest Assignment_A -- --template vue-ts 
+run in terminal to start: npm run dev
---
+++ Aufgabe1/Assignment_A/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { ref, computed } from 'vue';
+
+  const count = ref<number>(0)
+  const doubleCount = computed(() => count.value * 2) 
+
+  const increment = () => { count.value++ }
+  const decrement = () => { count.value-- }
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <p>Count: {{ count }}</p>
+  <p>Double Count: {{ doubleCount }}</p>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+
+
